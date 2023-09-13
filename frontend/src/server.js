74a47f22c5a1b0b901ef3e835a410d5af58a4efd
chaimyaham <chaim.yaham17@gmail.com>
2023-09-13 22:53:30 +0100

    backend url

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,2 @@
-export const server="http://localhost:8000/api/v2"
-export const backend_url = "http://localhost:8000/";
+export const server="https://sale-square-app.vercel.app/api/v2"
+export const backend_url = "https://sale-square-app.vercel.app/";
