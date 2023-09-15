3270159247039e438933315eb512f6decb7b77b2
chaimyaham <chaim.yaham17@gmail.com>
2023-09-15 21:33:30 +0100

    mod

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const path = require("path");
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'https://salesquare-app.vercel.app/',
+    origin: 'https://salesquare-app.vercel.app',
     credentials: true,
 }));
 app.use("/", express.static(path.join(__dirname,"./uploads")));
